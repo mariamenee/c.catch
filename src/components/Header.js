ee62cccaff6a1d2248fabda0ba5b0dffe6278a5c
Maria Renee <ytm8@txstate.edu>
2024-10-29 18:12:04 -0500

    header styles

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header className="app-header">
+            <div className="header-title">
+                <h1>TEXAS STATE UNIVERISTY</h1>
+            </div>
+            <div className="header-under"></div>
+            <div className="header-top"></div>
+        </header>
+    );
+};
+
+export default Header;
