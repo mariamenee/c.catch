ee6afebe5a279bc267df52a55e372e6698f73029
Maria Renee <ytm8@txstate.edu>
2024-10-30 13:27:10 -0500

    Finished header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from './favicon2.svg';
 import './Header.css';
 
 const Header = () => {
@@ -8,7 +9,15 @@ const Header = () => {
                 <h1>TEXAS STATE UNIVERISTY</h1>
             </div>
             <div className="header-under"></div>
-            <div className="header-top"></div>
+
+            <div className="logoLine">
+                <img className="icon" src={logo} alt="Logo" /> 
+                <div className="catch">.catch</div>
+                <div className="littleLine">|</div>
+                <div className="SLAC">STUDENT LEARNING ASSISTANCE CENTER</div>
+            </div>
+            <div className="header-line"></div>
+            <div className="vert-line"></div>
         </header>
     );
 };
